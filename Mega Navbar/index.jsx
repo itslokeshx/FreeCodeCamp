31e357231e75ffa0e2d22be217852910a6099063
itslokeshx <lokeshvijayraina@gmail.com>
2026-01-27 20:05:57 +0530

    Add initial implementation of reusable Navbar component with styling

---
+++ Mega Navbar/index.jsx
@@ -0,0 +1,18 @@
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="nav-item">
+          <a href='#'>Dashboard</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Widgets</a>
+        </li>
+        <li className="nav-item">
+          <button aria-expanded="false">Apps</button>
+        <ul aria-label ="Apps" className="sub-menu"></ul>  
+        </li>
+      </ul>
+    </nav>
+  )
+}
