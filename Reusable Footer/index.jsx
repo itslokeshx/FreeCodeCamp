99df4eca872bdec4baa5f6415df5baebbd3dc86a
itslokeshx <lokeshvijayraina@gmail.com>
2026-01-30 20:49:06 +0530

    Add reusable Footer component with initial implementation and styles; create Mega Navbar styles

--- Reusable Footer/index.jsx
+++ Reusable Footer/index.jsx
@@ -1,1 +1,31 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="unlist">
+        <ul>
+          <li>hii</li>
+          <li>hiii</li>
+        </ul>
 
+        <ul>
+          <li>hii</li>
+          <li>hiii</li>
+        </ul>
+
+        <ul>
+          <li>hii</li>
+          <li>hiii</li>
+        </ul>
+      </div>
+
+      <p> 2024 Fitness Dashboard. All Rights Reserved.</p>
+
+      <ul className="bottomList">
+        <li>hii</li>
+        <li>hii</li>
+        <li>hii</li>
+        <li>hii</li>
+      </ul>
+    </footer>
+  );
+};
