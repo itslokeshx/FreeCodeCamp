06a61efc9b870eb050c63000b85dfa82a02630cd
itslokeshx <lokeshvijayraina@gmail.com>
2026-04-04 17:47:40 +0530

    feat: implement search functionality in FruitsSearch component

--- React State,Hooks and Routing/fruit-search-app/index.js
+++ React State,Hooks and Routing/fruit-search-app/index.js
@@ -3,12 +3,33 @@ const { useState, useEffect } = React;
 export function FruitsSearch() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div id="search-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="search-input">Search for fruits:</label>
-        <input id="search-input" type="search" />
+        <input
+          id="search-input"
+          type="search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
       </form>
+      <div id="results">
+        {results.length > 0 ? (
+          results.map((item, index) => (
+            <p className="result-item" key={index}>
+              {item}
+            </p>
+          ))
+        ) : (
+          <p>No results found</p>
+        )}
+      </div>
     </div>
   );
 }
