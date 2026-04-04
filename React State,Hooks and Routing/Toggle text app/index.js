ecd0ee132743d6497f3fdbeb2c75aceeedac82d4
itslokeshx <lokeshvijayraina@gmail.com>
2026-04-04 12:40:01 +0530

    feat: implement handleToggleVisibility function and update button for message display in ToggleApp component

--- React State,Hooks and Routing/Toggle text app/index.js
+++ React State,Hooks and Routing/Toggle text app/index.js
@@ -3,11 +3,15 @@ const { useState } = React;
 export const ToggleApp = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  const handleToggleVisibility = () => {};
+  const handleToggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
 
   return (
     <div id="toggle-container">
-      <button id="toggle-button">Message</button>
+      <button onClick={handleToggleVisibility} id="toggle-button">
+        {isVisible ? "Hide" : "Show"} Message
+      </button>
       {isVisible && <p id="message">I love freeCodeCamp!</p>}
     </div>
   );
