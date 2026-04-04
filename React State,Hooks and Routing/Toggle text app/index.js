83491e802124b1b8af471596058a4e1cc9aeb5ef
itslokeshx <lokeshvijayraina@gmail.com>
2026-04-04 12:19:08 +0530

    feat: implement ToggleApp component with button and message display

--- React State,Hooks and Routing/Toggle text app/index.js
+++ React State,Hooks and Routing/Toggle text app/index.js
@@ -1,5 +1,10 @@
 const { useState } = React;
 
 export const ToggleApp = () => {
-  return <div id="toggle-container"></div>;
+  return (
+    <div id="toggle-container">
+      <button id="toggle-button">Message</button>
+      <p id="message">I love freeCodeCamp!</p>
+    </div>
+  );
 };
