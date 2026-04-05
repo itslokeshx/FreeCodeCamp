8dafc0570f043e77c89dd1eed6a5a028985f2374
itslokeshx <lokeshvijayraina@gmail.com>
2026-04-05 12:50:01 +0530

    feat: enhance SuperheroForm with Real Name input and power source selection

--- React State,Hooks and Routing/superhero application form/index.jsx
+++ React State,Hooks and Routing/superhero application form/index.jsx
@@ -20,7 +20,34 @@ export const SuperheroForm = () => {
               onChange={(e) => setHeroName(e.target.value)}
             />
           </label>
+          <label>
+            Real Name
+            <input
+              type="password"
+              value={realName}
+              onChange={(e) => setRealName(e.target.value)}
+            />
+          </label>
         </div>
+        <label className="section column">
+          How did you get your powers?
+          <select>
+            <option value="">Select one</option>
+
+            <option value="Bitten by a strange creature">
+              Bitten by a strange creature
+            </option>
+
+            <option value="Radioactive exposure">Radioactive exposure</option>
+            <option value="Science experiment">Science experiment</option>
+            <option value="Alien heritage ">Alien heritage</option>
+
+            <option value="Ancient artifact discovery">
+              Ancient artifact discovery
+            </option>
+            <option value="Other">Other</option>
+          </select>
+        </label>
       </form>
     </div>
   );
