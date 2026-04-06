04ae046c69a17adcada4b89400f2777d3b3379e1
itslokeshx <lokeshvijayraina@gmail.com>
2026-04-06 21:53:28 +0530

    feat: add items array to Shopping List component for item filtering

--- React State,Hooks and Routing/Shopping List/index.jsx
+++ React State,Hooks and Routing/Shopping List/index.jsx
@@ -1,18 +1,34 @@
 const { useState } = React;
 
+const items = [
+  "Apples",
+  "Bananas",
+  "Strawberries",
+  "Blueberries",
+  "Mangoes",
+  "Pineapple",
+  "Lettuce",
+  "Broccoli",
+  "Paper Towels",
+  "Dish Soap",
+];
+
 export const ShoppingList = () => {
+  const [query, setQuery] = useState("");
+
   return (
     <div className="container">
       <h1>Shopping List</h1>
       <form>
         <label htmlFor="search">Search for an item:</label>
         <input
-          type="search"
           id="search"
+          type="search"
           placeholder="Search..."
           aria-describedby="search-description"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
-
         <p id="search-description">Type to filter the list below:</p>
       </form>
     </div>
