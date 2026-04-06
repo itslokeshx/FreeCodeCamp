8309cee896559b9eb91fb6ac898c7403c5454d50
itslokeshx <lokeshvijayraina@gmail.com>
2026-04-06 21:49:08 +0530

    feat: add initial structure and search functionality to Shopping List component

--- React State,Hooks and Routing/Shopping List/index.jsx
+++ React State,Hooks and Routing/Shopping List/index.jsx
@@ -3,8 +3,18 @@ const { useState } = React;
 export const ShoppingList = () => {
   return (
     <div className="container">
-      <h1></h1>
-      <form></form>
+      <h1>Shopping List</h1>
+      <form>
+        <label htmlFor="search">Search for an item:</label>
+        <input
+          type="search"
+          id="search"
+          placeholder="Search..."
+          aria-describedby="search-description"
+        />
+
+        <p id="search-description">Type to filter the list below:</p>
+      </form>
     </div>
   );
 };
